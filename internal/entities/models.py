1f9878e4c71f48e6df7b0f51910a51ef58171bb0
ebo_shit <isakovn2005@gmail.com>
2025-07-24 18:59:28 +0300

    update steps

--- internal/entities/models.py
+++ internal/entities/models.py
@@ -7,8 +7,10 @@ class Participant(BaseModel):
     id: int | str = Field(alias="id")
     city: int | str = Field(alias="город")
     full_name: int | str = Field(alias="фио")
-    age: int | str = Field(alias="возраст")
-    position: int | str = Field(alias="должность")            # обратите внимание на пробел в ключе
+    closer_choice: int | str = Field(alias="Что вам ближе")
+    preferred_pattern: int | str = Field(alias="Какой образ вам ближе")
+    attraction_mode: int | str = Field(alias="больше привлекает")
+    metaphor_preference: int | str = Field(alias="метафора")
     ai_level: int | str= Field(alias="уровень в ИИ")
     team: int | str = Field(alias="команда")
 
